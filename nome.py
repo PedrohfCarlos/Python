ecb7301245299c33ad14c14cbd9c049868732d30
Pedro Henrique Filgueira Carlos <128182599+PedrohfCarlos@users.noreply.github.com>
2024-02-02 20:29:34 +0000

    v1

---
+++ nome.py
@@ -0,0 +1,11 @@
+
+print("Ola, qual é o seu nome?")
+nome = input() 
+print(f"ola {nome}, como voce tá ? ")
+res = input()
+if res == "sim" :
+    print("Que bom!")
+if res == "nao" :
+    print("Que pena!")
+else :
+    print("erro")
